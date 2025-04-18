6fbafe81a8865cc29c514c9c764a64191270e520
yahyaelganayni1 <yahyaelganayni@gmail.com>
2025-04-18 13:44:28 +0200

    Implement Article model with validations and search method; add ArticlesController and ArticlesHelper skeletons; create ArticlesControllerTest

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,14 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :content, presence: true
+  validates :author, length: { maximum: 100 }, allow_blank: true
+
+
+  def self.search(query)
+    if query.present?
+      where("title ILIKE :query OR content ILIKE :query OR author ILIKE :query", query: "%#{query}%")
+    else
+      all
+    end
+  end
 end
