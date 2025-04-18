5271dd5f1ed67eab6fdb8db7d931e62ab6947093
yahyaelganayni1 <yahyaelganayni@gmail.com>
2025-04-18 15:14:00 +0200

    Implement search functionality in articles index; log search queries

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,21 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    query = params[:query].to_s.strip
+    @articles = query.present? ? Article.search(query) : Article.all
+
+    # Log the search query if one was provided
+    log_search(query) if query.present?
   end
 
   private
 
-  def article_params
-    params.require(:article).permit(:title, :content, :author, :category, :tags, :published_at)
+  def log_search(query)
+    SearchLog.create(
+      search_query: query,
+      user_ip: request.remote_ip
+    )
+  rescue => e
+    # Just log the error and continue - don't disrupt the user experience
+    Rails.logger.error("Failed to log search: #{e.message}")
   end
 end
