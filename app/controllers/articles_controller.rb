9d18affc8711fbc5a4cf9983a0e9bd166cd003e0
yahyaelganayni1 <yahyaelganayni@gmail.com>
2025-04-18 15:28:35 +0200

    Add pagination to articles index; integrate Kaminari for page management

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,14 @@
 class ArticlesController < ApplicationController
   def index
     query = params[:query].to_s.strip
-    @articles = query.present? ? Article.search(query) : Article.all
+    @page = params[:page] || 1
+    @per_page = params[:per_page] || 10
+
+    @articles = if query.present?
+                  Article.search(query).page(@page).per(@per_page)
+    else
+                  Article.page(@page).per(@per_page)
+    end
 
     # Log the search query if one was provided
     log_search(query) if query.present?
